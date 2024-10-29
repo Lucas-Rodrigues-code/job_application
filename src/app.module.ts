2828bcb1cc5a398cf64ec3cc7f5f074453c8ae81
Lucas Rodrigues de Souza <Rodriguesdesouzal28@gmail.com>
2024-10-29 11:14:23 -0300

    user para modulo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,33 +1,16 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserController } from './application/user/controllers/user.controller';
-import { UserService } from './domain/user/services/user.service';
-import { UserRepository } from './infrastructure/repositories/user.repository';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
-import { JobApplicationController } from './application/job_application/controllers/job_application.controller';
-import { JobApplicationService } from './domain/job_application/services/job_application.service';
-import { JobApplicationRepository } from './infrastructure/repositories/job_application.repository';
-import { NetworkService } from './domain/network/services/network.service';
-import { NetworkRepository } from './infrastructure/repositories/network.repository';
-import { NetworkController } from './application/network/controllers/network.controller';
+import { UserModule } from './user/user.module';
+import { APP_PIPE } from '@nestjs/core';
 
 @Module({
-  imports: [DatabaseModule],
-  controllers: [
-    AppController,
-    UserController,
-    JobApplicationController,
-    NetworkController,
-  ],
+  imports: [DatabaseModule, UserModule],
+
   providers: [
-    AppService,
-    UserService,
-    UserRepository,
-    JobApplicationService,
-    JobApplicationRepository,
-    NetworkService,
-    NetworkRepository,
+    {
+      provide: APP_PIPE,
+      useClass: ValidationPipe,
+    },
   ],
 })
 export class AppModule {}
